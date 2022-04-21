4cf932c68dfea02d79edfa2ebfecf23a15e05479
norak <khalil.nor@northeastern.edu>
2022-04-21 11:02:38 -0400

    now using csv.DictReader, and creating a dict of results

--- refrigerants/experimental_data/experimental_lists.py
+++ refrigerants/experimental_data/experimental_lists.py
@@ -1,24 +1,28 @@
 import os
 import re
+import csv
 
 directory = '.'
-d= os.path.join(directory,'experimental_data.csv')
+d = os.path.join(directory, 'experimental_data.csv')
+
+data = dict()
 
 with open(d,'r') as csvfile: 
-    lines = csvfile.readlines()
-    for line in lines[1:]:
-        match = re.match('R-[a-zA-Z0-9]+,[^,]+,([\w]+)[\s+]?,+[0-9.]+,[0-9.]+,([0-9.]+),([0-9.]+),',line)
-        #match = re.match('R-[a-zA-Z0-9]+,[A-Z()=]+,([\w]+),[0-9.]+,[0-9.]+,([0-9.]+),([0-9.]+),',line)
-        if match: 
-            group, vol_frac, flamespeed = (match.group(1)), float(match.group(2)), float(match.group(3))
-            print(group, vol_frac, flamespeed)
-        else: 
-            print('code better Nora')
- 
+    csvreader = csv.DictReader(csvfile)
 
+    for row in csvreader:
+        formula = row['formula']
+        vol_frac = row['volume_frac']
+        flamespeed = row['Su (cm/s)']
 
-
-
+        if formula not in data:
+            data[formula] = ([], [])  # tuple of two lists, one for volume fractions, one for flame speeds
+        data[formula][0].append(vol_frac)
+        data[formula][1].append(flamespeed)
+        
+        print(formula, vol_frac, flamespeed)
+    
+print(data)
 
 
     #R-32,FCF,CH2F2,0.1736,0.9,0.15624,5.97,air,298,1,CVM,10,"T
