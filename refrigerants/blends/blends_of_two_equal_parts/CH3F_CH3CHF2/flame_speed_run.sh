c74d135325297579a084ec680b69fdec37b36f76
Nora Khalil <khalil.nor@northeastern.edu>
2023-01-31 10:26:49 -0500

    updates

---
+++ refrigerants/blends/blends_of_two_equal_parts/CH3F_CH3CHF2/flame_speed_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=7-00:00:00
+#SBATCH --job-name=fc_CH3F(1)_CH3CHF2(2)
+#SBATCH --error=error_fc.slurm.log
+#SBATCH --output=output_fc.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+##SBATCH --array=1
+#SBATCH --partition=west
+
+source activate cantera_env
+python flame_speed_calc.py
+
