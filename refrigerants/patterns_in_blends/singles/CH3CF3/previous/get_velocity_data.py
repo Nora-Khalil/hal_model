7d9188e5fa2978f5a418e19df89c7f0e1c6a74cf
Nora Khalil <khalil.nor@northeastern.edu>
2023-01-31 10:11:54 -0500

    updates

---
+++ refrigerants/patterns_in_blends/singles/CH3CF3/previous/get_velocity_data.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+directory = './data'
+
+file_names = os.listdir(directory)
+
+file_name_dict_unsorted = {}
+
+for name in file_names:
+    match = re.match('test_([0-9.]+)\.csv', name)
+    if match:
+        volume_fraction = float(match.group(1))
+        file_name_dict_unsorted[name] = volume_fraction
+    else:
+        print(f"didn't match {name}")
+
+volume_fractions_float = sorted(file_name_dict_unsorted.values())
+
+    
+print('Volume fractions are:')
+print(volume_fractions_float)
+
+
+#makes list of velocities 
+
+flamespeeds = []
+
+filenames_sorted_dict = {k:v for k,v in sorted(file_name_dict_unsorted.items(), key=lambda item: item[1])}
+for file in filenames_sorted_dict.keys():
+    d = os.path.join(directory,file)    
+    with open(d,'r') as csvfile:
+        match = re.match('0,0\.0,([0-9.]+),298\.0',csvfile.readlines()[1])
+        flamespeeds.append(float(match.group(1)))
+print('Flame speeds are') 
+print(flamespeeds)
+        
---
+++ refrigerants/singles/Burgess_Comments/methane_with_added_2_BTP/compare/120_140_species/run_range_parallel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=one_flip_130-129
+#SBATCH --partition=west
+#SBATCH --exclude=c5003
+#SBATCH --mem=20Gb
+#SBATCH --time=3-00:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --array=1
+
+
+# Define useful bash variables
+SLURM_TASK_ID_OFFSET=0
+
+selected_files=(chem_840-from-June-file.cti)
+
+index=$SLURM_ARRAY_TASK_ID-1
+
+folder_name="${selected_files[$index]}"
+
+my_path="/work/westgroup/nora/Code/projects/halogens/refrigerants/singles/Burgess_Comments/compare/handpicked/handpicked_models/${folder_name}"
+
+
+python -u  flame_speed_calc.py $my_path
